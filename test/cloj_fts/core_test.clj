90d30c6f81f29f04929f06d42ab9662d66ed123c
Abhishek Gupta <abhii.cse.nitdgp@gmail.com>
2017-05-15 18:09:59 +0530

    Added some tests

---
+++ test/cloj_fts/core_test.clj
@@ -0,0 +1,28 @@
+(ns cloj-fts.core-test
+  (:require [clojure.test :refer :all]
+            [cloj-fts.core :refer :all]))
+
+
+
+(deftest test-next-id
+  (let [ig in-mem-id-generator]
+    (is (not (nil? (next-id ig)))
+        )
+    ))
+
+
+
+(deftest test-search
+  (let [query "we ARE"]
+    (is (= (search query)
+           '()))
+    (do (index-doc (create-doc "We are going to build a HUUUUUGEEE wall")))
+    (is (= (count (search query))
+           1))
+    (do (index-doc (create-doc "YES we ARe")))
+    (is (= (count (search query))
+           2))
+    ))
+
+
+
